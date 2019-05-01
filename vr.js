dbc19d67ae05b0b23181c73183fa6dc008617cc8
jkmosa01 <35510468+jkmosa01@users.noreply.github.com>
2019-05-01 15:59:04 -0600

    fixed this

--- vr.js
+++ vr.js
@@ -4,8 +4,8 @@ function handleOrientation(event) {
     var y = event.gamma; //side to side
     var z = event.alpha; //twist
     // alert(x+" "+y+" "+z);
-    camera.rotation.x = event.beta * Math.PI/180;
-    camera.rotation.y = event.gamma * Math.PI/180;
-    camera.rotation.y = event.alpha * Math.PI/180;
+    camera.rotation.x = -event.beta * Math.PI/180;
+    camera.rotation.y = -event.gamma * Math.PI/180;
+    camera.rotation.y = -event.alpha * Math.PI/180;
 }
 window.addEventListener('deviceorientation', handleOrientation);
