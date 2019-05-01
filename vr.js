00cdf8b8d621bb8e36e58095d8f5611d9e93e8ef
jkmosa01 <35510468+jkmosa01@users.noreply.github.com>
2019-05-01 17:25:19 -0600

    added console logging

--- vr.js
+++ vr.js
@@ -1,7 +1,8 @@
 function handleOrientation(event) {
+    console.log(event.beta.toFixed(2)+" "+event.gamma.toFixed(2)+" "+event.alpha.toFixed(2))
     camera.rotation.x = (event.beta-90) * Math.PI/180;
     camera.rotation.z = (event.gamma) * Math.PI/180;
     camera.rotation.y = -event.alpha * Math.PI/180;
 }
 window.addEventListener('deviceorientation', handleOrientation);
-alert("VR v0.2");
+alert("VR v0.3");
